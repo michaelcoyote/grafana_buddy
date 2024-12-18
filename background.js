a7aecba448997c4a7b0f84ffb51433695ba89101
michael guldan <michaelguldan@gmail.com>
2024-12-17 23:13:38 -0800

    Fix datetime function typo

--- background.js
+++ background.js
@@ -6,7 +6,7 @@ function getCurrentDate() {
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const day = String(date.getDate()).padStart(2, '0');
-    const hour = String(date.getHour()).padStart(2, '0');
+    const hour = String(date.getHours()).padStart(2, '0');
     const min = String(date.getMinutes()).padStart(2, '0');
 
     return `${year}_${month}_${day}__${hour}${min}`;
